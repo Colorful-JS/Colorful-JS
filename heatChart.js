6ba817d9aa3c851b4a0cc7800d6eaf6066e42264
Lawson Kurtz <lawson.kurtz@gmail.com>
2012-09-18 21:38:57 -0400

    refactored class methods a bit

--- heatChart.js
+++ heatChart.js
@@ -28,23 +28,24 @@
 		    if(s == 0){
 		        r = g = b = l;
 		    } else {
-		        function hue2rgb(p, q, t){
-		            if(t < 0) t += 1;
-		            if(t > 1) t -= 1;
-		            if(t < 1/6) return p + (q - p) * 6 * t;
-		            if(t < 1/2) return q;
-		            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-		            return p;
-		        }
+		        
 		
 		        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
 		        var p = 2 * l - q;
-		        r = hue2rgb(p, q, h + 1/3);
-		        g = hue2rgb(p, q, h);
-		        b = hue2rgb(p, q, h - 1/3);
+		        r = methods.hue2rgb(p, q, h + 1/3);
+		        g = methods.hue2rgb(p, q, h);
+		        b = methods.hue2rgb(p, q, h - 1/3);
 		    }
 
 		    return [r * 255, g * 255, b * 255];
+		},
+		hue2rgb : function (p, q, t){
+		    if(t < 0) t += 1;
+		    if(t > 1) t -= 1;
+		    if(t < 1/6) return p + (q - p) * 6 * t;
+		    if(t < 1/2) return q;
+		    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		    return p;
 		}
 	}
 
